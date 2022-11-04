07fc2dfb3cc74238575998571e1496e264dbc476
Robin Bette <robinbette@hotmail.com>
2022-11-04 17:42:03 +0100

    * refactor slightly by returning early

--- src/ACM.BL/Customer.cs
+++ src/ACM.BL/Customer.cs
@@ -5,20 +5,17 @@ public class Customer
     public string EmailAddress { get; set; } = string.Empty;
     public string FirstName { get; set; } = string.Empty;
     public string LastName { get; set; } = string.Empty;
-	public string FullName
-	{
+    public string FullName
+    {
         get
         {
             var fullName = LastName;
-            if (!string.IsNullOrWhiteSpace(FirstName))
-            {
-                if (!string.IsNullOrWhiteSpace(fullName))
-                {
-                    fullName += ", ";
-                }
-                fullName += FirstName;
-            }
-            return fullName;
+            if (string.IsNullOrWhiteSpace(FirstName))
+                return fullName;
+
+            return string.IsNullOrWhiteSpace(fullName)
+                ? FirstName
+                : fullName += $", {FirstName}";
         }
-	}
+    }
 }
