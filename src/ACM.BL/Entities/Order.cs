528f388bea682ac9a8a4db47adab289ca89b80c3
Robin Bette <robinbette@hotmail.com>
2022-11-07 12:24:27 +0100

    * use composition by id vs property as a reference

--- src/ACM.BL/Entities/Order.cs
+++ src/ACM.BL/Entities/Order.cs
@@ -1,12 +1,23 @@
 ﻿namespace ACM.BL.Entities;
 public class Order
 {
-    public Order() { }
+    public Order() : this(0) { }
     public Order(int id)
     {
         Id = id;
+        OrderItems = new List<OrderItem>();
     }
 
     public int Id { get; private set; }
+
+    /// <summary>
+    /// Use composition by using a customer id and shipping address id
+    /// The order does not care about the details of these objects
+    /// So we do not use property as a reference to the customer, 
+    /// which then loads all the details of the customer
+    /// </summary>
+    public int CustomerId { get; set; }
+    public int ShippingAddressId { get; set; }
     public DateTimeOffset? OrderDate { get; set; }
+    public List<OrderItem> OrderItems { get; set; }
 }
