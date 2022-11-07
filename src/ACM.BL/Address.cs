435cc970d90842a0307822ee109bee10dcd854bd
Robin Bette <robinbette@hotmail.com>
2022-11-07 08:55:51 +0100

    * add address class

---
+++ src/ACM.BL/Address.cs
@@ -0,0 +1,24 @@
+﻿namespace ACM.BL
+{
+    public class Address
+    {
+        public Address() { }
+        public Address(int id)
+        {
+            Id = id;
+        }
+
+        public int Id { get; private set; }
+        public int AddressType { get; set; }
+        public string StreetLine1 { get; set; } = string.Empty;
+        public string StreetLine2 { get; set; } = string.Empty;
+        public string City { get; set; } = string.Empty;
+        public string StateOrProvince { get; set; } = string.Empty;
+        public string PostalCode { get; set; } = string.Empty;
+        public string Country { get; set; } = string.Empty;
+        public string Validate { get; set; } = string.Empty;
+
+        public bool IsValid() =>
+            !string.IsNullOrWhiteSpace(PostalCode);
+    }
+}
